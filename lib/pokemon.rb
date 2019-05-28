1e458777a4a9568f5a5059b0622276202912339a
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-28 02:29:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", names, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
   def self.find(id_num, db)
